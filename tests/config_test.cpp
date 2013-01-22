2184baf842bd73d30287d4c74217df7e42591509
Nathan Campos <eeepc904@gmail.com>
2013-01-22 13:19:26 -0200

    #14 - Improve the Config unit tests to reflect the recent additions

--- tests/config_test.cpp
+++ tests/config_test.cpp
@@ -18,3 +18,28 @@ TEST_F(ConfigTest, CheckDirectories) {
         config.check_dirs("history");
     });
 }
+
+TEST_F(ConfigTest, LoadUserAliasConfiguration) {
+    ASSERT_NO_THROW({
+        config.load_user_config("freenode");
+    });
+    
+    EXPECT_EQ("irc.freenode.net", config.server_location);
+    EXPECT_EQ("leafirc", config.user_nick);
+    EXPECT_EQ("leafirc", config.user_username);
+    EXPECT_EQ("LeafIRC", config.user_realname);
+    //std::vector<std::string> channels;
+}
+
+TEST_F(ConfigTest, LoadUserNonAliasConfiguration) {
+    ASSERT_NO_THROW({
+        config.load_user_config("localhost");
+    });
+
+    EXPECT_EQ("localhost", config.server_location);
+    EXPECT_EQ("leafirc", config.user_nick);
+    EXPECT_EQ("leafirc", config.user_username);
+    EXPECT_EQ("LeafIRC", config.user_realname);
+    EXPECT_EQ("", config.user_password);
+    //std::vector<std::string> channels;
+}

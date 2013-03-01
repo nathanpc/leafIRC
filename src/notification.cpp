9b6763e3689c2f9553222af732e474a48b00b6b9
Nathan Campos <eeepc904@gmail.com>
2013-03-01 10:02:30 -0300

    #22 - Implemented (untested) Linux support for notifications using libnotify

--- src/notification.cpp
+++ src/notification.cpp
@@ -11,7 +11,9 @@
 #ifdef __APPLE__
 	#include <growl.h>
 #else
-	#include <growl.h>
+	#include <glib.h>
+	#include <unistd.h>
+	#include <libnotify/notify.h>
 #endif
 
 #include "notification.h"
@@ -19,11 +21,26 @@ using namespace std;
 
 int Notification::notify(string title, string message) {
 	#ifdef __APPLE__
+		// Growl for Mac.
 		growl_init();
 		int rc = growl("127.0.0.1", "leafirc", "leafirc", title.c_str(), message.c_str(), NULL, NULL, NULL);
 		growl_shutdown();
 	#else
-		// libnotify.
+		// libnotify for UNIX.
+		NotifyNotification *notification;
+		gboolean success;
+		GError *error;
+
+		if (!notify_init("leafIRC")) {
+			exit(EXIT_FAILURE);
+		}
+
+		error = NULL;
+		notification = notify_notification_new(title.c_str(), message.c_str(), NULL);
+		success = notify_notification_show(notification, &error);
+		notify_uninit();
+
+		return (int)success;
 	#endif
 
 	return rc;
